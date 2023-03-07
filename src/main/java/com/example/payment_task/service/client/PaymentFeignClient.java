93123c353a8fea394dbe82e6c1fb000eef04055c
Andrei <binofaata@gmail.com>
2023-03-07 10:22:01 +0200

    fix property constants

--- src/main/java/com/example/payment_task/service/client/PaymentFeignClient.java
+++ src/main/java/com/example/payment_task/service/client/PaymentFeignClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(url = "https://app-demo.paze.eu/api/v1", name = "paymentService",
+@FeignClient(url = "${feign.services.paymentService.url}", name = "paymentService",
         configuration = FeignClientProperties.FeignClientConfiguration.class)
 public interface PaymentFeignClient {
     @PostMapping("/payments")
